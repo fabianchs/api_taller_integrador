ed4d2232d7d7f6533aed658a737f9f9fd276cdf0
chackymeow <fabichasola@hotmail.com>
2024-10-01 14:27:43 +0000

    first steps

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,26 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Measure(db.Model):
+
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    position = db.Column(db.String(240), unique=False, nullable=True)
+    date = db.Column(db.String(120), unique=False, nullable=True)
+    temperature = db.Column(db.String(120),unique=False, nullable=True)
+    hour = db.Column(db.String(120),unique=False, nullable=True)
+
+
+    def __repr__(self):
+        return '<Data %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "position": self.position,
+            "date": self.date,
+            "temperature": self.temperature,
+            "hour": self.hour
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Measure
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -20,3 +20,28 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+    
+
+@api.route("/send_all", methods=["GET"])
+def all_measures():
+
+    dictionary= {"message" : "hello world"}
+
+    measure=Measure.query.all()
+    all_measures = list(map(lambda x: x.serialize(), measure))
+
+    return jsonify(all_measures), 200
+
+@api.route('/newmeasure', methods=['POST'])
+def new_measure():
+
+    request_body=request.get_json()
+    
+    new_measure = Measure(position=request_body["position"], date=request_body["date"], temperature=request_body["temperature"], hour=request_body["hour"])
+
+    db.session.add(new_measure)
+    db.session.commit()
+
+    return jsonify("Measure added correctly"), 200
